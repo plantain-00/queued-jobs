0e79348a3d5635d6ed2057f51c436441f7ae7af4
york yao <yaoao12306@outlook.com>
2018-01-21 17:52:34 +0800

    test: queue overflow

--- spec/indexSpec.ts
+++ spec/indexSpec.ts
@@ -32,8 +32,29 @@ it('timeout', async () => {
   await queuedJobs.handle('timeout').then(result => {
     fail()
   }).catch((error: Error) => {
-    console.log({ error })
     expect(error.message).toEqual('timeout')
+  })
+})
+
+it('queue overflow', async () => {
+  const queuedJobs = new QueuedJobs<string, string>(1)
+  queuedJobs.registerHandler(async (data) => {
+    await sleep(1000)
+    return data + '-' + data
+  })
+
+  queuedJobs.handle('queue overflow 0').then(result => {
+    expect(result).toEqual('queue overflow 0-queue overflow 0')
+  })
+
+  queuedJobs.handle('queue overflow 1').then(result => {
+    fail()
+  }).catch((error: Error) => {
+    expect(error.message).toEqual('queue overflow')
+  })
+
+  await queuedJobs.handle('queue overflow 2').then(result => {
+    expect(result).toEqual('queue overflow 2-queue overflow 2')
   })
 })
 
