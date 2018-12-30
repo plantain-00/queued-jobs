4887bb6da59095da8edbf7385931eb0d08fb34a7
york yao <yaoao12306@outlook.com>
2018-12-30 08:21:13 +0800

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,9 @@ import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: 'dist/browser/browser.js',
-  name: 'QueuedJobs',
   plugins: [resolve({ browser: true }), uglify()],
   output: {
+    name: 'QueuedJobs',
     file: 'dist/queued-jobs.min.js',
     format: 'umd'
   }
